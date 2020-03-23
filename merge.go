3d7336a1562e04adea24a9f4fae8912f6286c8b5
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-03-23 14:22:37 -0500

    Make behavior on failure more user-friendly by not panicking. Provide exit status on command failure.

--- merge.go
+++ merge.go
@@ -6,13 +6,14 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 	"sync"
-	"regexp"
 )
 
 const COLOR_FORMAT string = "\033[3%dm%s\033[0m"
 const NUM_COLORS = 6
+
 var TRIM_COLORS_PATTERN *regexp.Regexp
 
 var messageBuffer = make(chan message)
@@ -35,20 +36,28 @@ func readPipe(pipe io.ReadCloser, mergedOutput chan string, wait *sync.WaitGroup
 	}
 }
 
+func identifier(command *exec.Cmd) string {
+	fullId := strings.Join(command.Args, " ")
+	if len(fullId) <= 60 {
+		return fullId
+	}
+	return fullId[:60] + "..."
+}
+
 // Sends both stdout and stderror of command to mergedOutput
 func mergeOutErr(command *exec.Cmd, mergedOutput chan string) {
 	stdout, err := command.StdoutPipe()
 	if err != nil {
-		panic(fmt.Sprintf("Could not connect to stdout of process '%s'. Error: %s", command.Path, err))
+		fatal(fmt.Sprintf("Could not connect to stdout of process '%s'. Details: %s", identifier(command), err))
 	}
 	stderror, err := command.StderrPipe()
 	if err != nil {
-		panic(fmt.Sprintf("Could not connect to stderr of process '%s'. Error: %s", command.Path, err))
+		fatal(fmt.Sprintf("Could not connect to stderr of process '%s'. Details: %s", identifier(command), err))
 	}
 	if err := command.Start(); err != nil {
-		panic(fmt.Sprintf("Could not start process '%s'. Error: %s", command.Path, err))
+		fatal(fmt.Sprintf("Could not start process '%s'. Details: %s", identifier(command), err))
 	} else {
-		mergedOutput <- fmt.Sprintf("Started '%s' (%d)", command.Path, command.Process.Pid)
+		mergedOutput <- fmt.Sprintf("Started '%s' (%d)", identifier(command), command.Process.Pid)
 	}
 	var both sync.WaitGroup
 	both.Add(2)
@@ -65,17 +74,25 @@ func listenTo(command *exec.Cmd, id int) {
 	for line := range mergedOutput {
 		messageBuffer <- message{line, id}
 	}
-	if err := command.Wait(); err != nil {
-		messageBuffer <- message{fmt.Sprintf("Process '%s' (%d) exited with %s", command.Path, command.Process.Pid, err), id}
+	if err, ok := (command.Wait()).(*exec.ExitError); err != nil {
+		if ok {
+			messageBuffer <- message{fmt.Sprintf("Process '%s' (%d) exited with status %d", identifier(command), command.Process.Pid, err.ExitCode()), id}
+		} else {
+			messageBuffer <- message{fmt.Sprintf("Process '%s' (%d) exited abnormally. Details: %s", identifier(command), command.Process.Pid, err), id}
+		}
 	} else {
-		messageBuffer <- message{fmt.Sprintf("Process '%s' (%d) exited successfully", command.Path, command.Process.Pid), id}
+		messageBuffer <- message{fmt.Sprintf("Process '%s' (%d) exited successfully", identifier(command), command.Process.Pid), id}
 	}
+}
+
+func fatal(errorMessage string) {
+	fmt.Fprintln(os.Stderr, fmt.Sprintf("Error: %s", errorMessage))
+	os.Exit(1)
 }
 
 func main() {
 	if len(os.Args) < 3 {
-		fmt.Fprintln(os.Stderr, "Error: must supply at least two processes to run")
-		os.Exit(1)
+		fatal("Must supply at least two processes to run")
 	}
 	TRIM_COLORS_PATTERN = regexp.MustCompile("\033\\[[^m]*m")
 	processWait.Add(len(os.Args) - 1)
